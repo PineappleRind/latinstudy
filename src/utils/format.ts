9a6281991559e5c74185c5c200b9ad4f8bdee9c0
pr <pineapplerind.info@gmail.com>
2023-09-15 13:01:15 -0400

    remove titleCase & border-strong, unused

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,9 +4,3 @@ export const ord = (n: string) =>
 	{ e: "st", o: "nd", w: "rd", h: "th" }[
 		new Intl.PluralRules("en", { type: "ordinal" }).select(parseInt(n))[2]
 	];
-
-export const titleCase = (str: string) =>
-	str
-		.split(" ")
-		.map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase())
-		.join(" ");
